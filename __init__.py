c5858f9b1100e133a9ac02ef4b2eb63a6a6912dd
Benjamin Golder <benjamin.j.golder@gmail.com>
2012-12-24 23:53:55 -0500

    built out and tested Matrix class

--- __init__.py
+++ __init__.py
@@ -1,13 +1,18 @@
-from core import (
+from vector import (
         Vector3d,
         Point3d,
         PointSet,
+)
+from matrix import (
+        Matrix,
+        MatrixError,
 )
 
 __all__ = [
         'Vector3d',
         'Point3d',
         'PointSet',
+        'Matrix',
         ]
 
 

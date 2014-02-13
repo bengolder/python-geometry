adb73d2cf7520be5fe69a1fd4526d913267edc7f
Benjamin Golder <benjamin.j.golder@gmail.com>
2014-02-13 09:08:38 -0500

    adding basic test

---
+++ tests/test_imports.py
@@ -0,0 +1,31 @@
+import unittest2 as unittest
+
+class TestImport(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_imports(self):
+        import sys
+        sys.path.append("./..")
+        from geometry import (
+                Interval,
+                Scale,
+                Box2d,
+                Box3d,
+                Vector2d,
+                Vector3d,
+                Point2d,
+                Point3d,
+                PointSet,
+                PageX,
+                PageY,
+                WorldX,
+                WorldY,
+                WorldZ,
+                Matrix,
+                Line3d,
+                LineSegment2d,
+                Plane3d,
+        )
+
+

a18bcc8a89541ec2c2a5963623e1fe82df66d324
Benjamin Golder <benjamin.j.golder@gmail.com>
2014-02-25 13:39:37 -0500

    cleaning comments

--- geometry/vector3d.py
+++ geometry/vector3d.py
@@ -5,10 +5,6 @@ class Vector3d(Vector2d):
     """A 3d vector object
     """
     def __init__(self, x=0.0, y=0.0, z=0.0):
-        # coords is the essence of the data structure. It's immutable and
-        # iterable, allowing us to iterate over the values as well as providing
-        # a little bit of protection from accidentally changing the values see
-        # `classTest` in tests to understand more of the reasoning here.
         Vector2d.__init__(self)
         self.coords = (x, y, z)
 
@@ -37,16 +33,6 @@ class Vector3d(Vector2d):
 
     def __getitem__(self, key):
         """Treats the vector as a tuple or dict for indexes and slicing.
-            >>> v
-            Vector3d(2.0, 1.0, 2.2)
-            >>> v[0]
-            2.0
-            >>> v[-1]
-            2.2000000000000002
-            >>> v[:2]
-            (2.0, 1.0)
-            >>> v['y']
-            1.0
         """
         # dictionary
         if key in ('x','y','z'):
@@ -57,12 +43,6 @@ class Vector3d(Vector2d):
 
     def cross(self, other):
         """Gets the cross product between two vectors
-            >>> v
-            Vector3d(5, 1.20747670785, 60.0)
-            >>> v1
-            Vector3d(0.0, 2.0, 1.0)
-            >>> v1.cross(v)
-            Vector3d(118.792523292, 5.0, -10.0)
         """
         x = (self[1] * other[2]) - (self[2] * other[1])
         y = (self[2] * other[0]) - (self[0] * other[2])
